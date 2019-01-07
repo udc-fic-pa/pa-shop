df529e4600dec6f3ce0f0f3a39c7022f922adc12
Fernando Bellas Permuy <fbellas@udc.es>
2019-01-07 14:32:37 +0100

    Frontend: se introduce el componente Products para simplificar la implementación del componente FindProductsResult.

--- frontend/src/modules/catalog/components/FindProductsResult.js
+++ frontend/src/modules/catalog/components/FindProductsResult.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
 import {FormattedMessage} from 'react-intl';
 
 import * as selectors from '../selectors';
 import * as actions from '../actions';
 import {Pager} from '../../common';
+import Products from './Products';
 
 const FindProductsResult = ({productSearch, categories, previousFindProductsResultPage, nextFindProductsResultPage}) => {
 
@@ -24,31 +24,7 @@ const FindProductsResult = ({productSearch, categories, previousFindProductsResu
     return (
 
         <div>
-
-            <table className="table table-striped table-hover">
-
-                <thead>
-                    <tr>
-                        <th scope="col">
-                            <FormattedMessage id='project.global.fields.department'/>
-                        </th>
-                        <th scope="col">
-                            <FormattedMessage id='project.global.fields.name'/>
-                        </th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {productSearch.result.items.map((product, index) => 
-                        <tr key={index}>
-                            <td>{selectors.getCategory(categories, product.categoryId).name}</td>
-                            <td><Link to={`/catalog/product-details/${product.id}/withBackLink`}>{product.name}</Link></td>
-                        </tr>
-                    )}
-                </tbody>
-
-            </table>
-
+            <Products products={productSearch.result.items} categories={categories}/>
             <Pager 
                 back={{
                     enabled: productSearch.criteria.page >= 1,
@@ -56,7 +32,6 @@ const FindProductsResult = ({productSearch, categories, previousFindProductsResu
                 next={{
                     enabled: productSearch.result.existMoreItems,
                     handle: () => nextFindProductsResultPage(productSearch.criteria)}}/>
-
         </div>
 
     );
---
+++ frontend/src/modules/catalog/components/Products.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {FormattedMessage} from 'react-intl';
+import PropTypes from 'prop-types';
+
+import * as selectors from '../selectors';
+
+const Products = ({products, categories}) => (
+
+    <table className="table table-striped table-hover">
+
+        <thead>
+            <tr>
+                <th scope="col">
+                    <FormattedMessage id='project.global.fields.department'/>
+                </th>
+                <th scope="col">
+                    <FormattedMessage id='project.global.fields.name'/>
+                </th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {products.map((product, index) => 
+                <tr key={index}>
+                    <td>{selectors.getCategory(categories, product.categoryId).name}</td>
+                    <td><Link to={`/catalog/product-details/${product.id}/withBackLink`}>{product.name}</Link></td>
+                </tr>
+            )}
+        </tbody>
+
+    </table>
+
+);
+
+Products.propTypes = {
+    products: PropTypes.array.isRequired,
+    categories: PropTypes.array.isRequired
+};
+
+export default Products;
