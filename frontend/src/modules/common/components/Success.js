2066d4e3ab0609124e0ed3ecdc390496aa2bbdc6
Fernando Bellas Permuy <fbellas@udc.es>
2019-01-07 10:37:07 +0100

    Frontend: ahora los componentes Loader y Success utilizan renderizado condicional para que el estilo sea más elegante.

--- frontend/src/modules/common/components/Success.js
+++ frontend/src/modules/common/components/Success.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Success = ({message, handleClose}) => {
-
-    if (!message) {
-        return null;
-    }
-
-    return (
-
-        <div className="alert alert-success alert-dismissible fade show" role="alert">
-            {message}
-            <button type="button" className="close" data-dismiss="alert" aria-label="Close" 
-                onClick={() => handleClose()}>
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-
-    );
-
-}
+const Success = ({message, handleClose}) => message && (
+    <div className="alert alert-success alert-dismissible fade show" role="alert">
+        {message}
+        <button type="button" className="close" data-dismiss="alert" aria-label="Close" 
+            onClick={() => handleClose()}>
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+);
 
 Success.propTypes = {
     message: PropTypes.string,
