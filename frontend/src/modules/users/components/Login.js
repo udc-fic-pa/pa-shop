8ad7efa0b1335320a2347ba154ee4933b98fe924
Fernando Bellas Permuy <fbellas@udc.es>
2019-01-11 14:23:29 +0100

    Autenticación: se añade la opción de recordar la autenticación.

--- frontend/src/modules/users/components/Login.js
+++ frontend/src/modules/users/components/Login.js
@@ -10,6 +10,7 @@ import * as actions from '../actions';
 const initialState = {
     userName: '',
     password: '',
+    rememberMe: false,
     backendErrors: null
 };
 
@@ -35,6 +36,10 @@ class Login extends React.Component {
 
     handlePasswordChange(event) {
         this.setState({password: event.target.value});
+    }
+
+    handleRememberMeChange() {
+        this.setState({rememberMe: !this.state.rememberMe});
     }
 
     handleSubmit(event) {
@@ -115,6 +120,17 @@ class Login extends React.Component {
                                         required/>
                                     <div className="invalid-feedback">
                                         <FormattedMessage id='project.global.validator.required'/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="form-group row">     
+                                <div className="offset-md-3 col-md-4">
+                                    <div className="form-check">
+                                        <input type="checkbox" id="rememberMe" className="form-check-input"
+                                            onChange={(e) => this.handleRememberMeChange(e)}/>
+                                        <label htmlFor="rememberMe" className="form-check-label">
+                                            Example checkbox
+                                        </label>
                                     </div>
                                 </div>
                             </div>
