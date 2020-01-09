417dba228aa535f8fd916a7c5d3caec5c72931fb
Fernando <fbellas@udc.es>
2020-01-09 19:35:18 +0100

    Frontend: FindProducts.js migrado a componente función.

--- frontend/src/modules/catalog/components/FindProducts.js
+++ frontend/src/modules/catalog/components/FindProducts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import {FormattedMessage} from 'react-intl';
@@ -6,62 +6,38 @@ import {FormattedMessage} from 'react-intl';
 import CategorySelector from './CategorySelector';
 import * as actions from '../actions';
 
-const initialState = {
-    categoryId: '',
-    keywords: ''
-};
+const FindProducts = ({dispatch, history}) => {
 
-class FindProducts extends React.Component {
+    const [categoryId, setCategoryId] = useState('');
+    const [keywords, setKeywords] = useState('');
 
-    constructor(props) {
-
-        super(props);
-
-        this.state = initialState;
-
-    }
-
-    handleCategoryIdChange(event) {
-        this.setState({categoryId: event.target.value});
-    }
-
-    handleKeywordsChange(event) {
-        this.setState({keywords: event.target.value});
-    }
-
-    handleSubmit(event) {
+    const handleSubmit = event => {
         event.preventDefault();
-        this.props.dispatch(actions.findProducts(
-            {categoryId: this.toNumber(this.state.categoryId), 
-                keywords: this.state.keywords.trim(), page: 0}));
-        this.props.history.push('/catalog/find-products-result');
+        dispatch(actions.findProducts(
+            {categoryId: toNumber(categoryId), 
+                keywords: keywords.trim(), page: 0}));
+        history.push('/catalog/find-products-result');
     }
 
-    toNumber(value) {
-        return value.length > 0 ? Number(value) : null;
-    }
+    const toNumber = value => value.length > 0 ? Number(value) : null;
 
-    render () {
+    return (
 
-        return (
+        <form className="form-inline mt-2 mt-md-0" onSubmit={e => handleSubmit(e)}>
 
-            <form className="form-inline mt-2 mt-md-0" onSubmit={e => this.handleSubmit(e)}>
+            <CategorySelector id="categoryId" className="custom-select my-1 mr-sm-2"
+                value={categoryId} onChange={e => setCategoryId(e.target.value)}/>
 
-                <CategorySelector id="categoryId" className="custom-select my-1 mr-sm-2"
-                    value={this.state.categoryId} onChange={e => this.handleCategoryIdChange(e)}/>
+            <input id="keywords" type="text" className="form-control mr-sm-2"
+                value={keywords} onChange={e => setKeywords(e.target.value)}/>
+            
+            <button type="submit" className="btn btn-primary my-2 my-sm-0">
+                <FormattedMessage id='project.global.buttons.search'/>
+            </button>
 
-                <input id="keywords" type="text" className="form-control mr-sm-2"
-                    value={this.state.keywords} onChange={e => this.handleKeywordsChange(e)}/>
-                
-                <button type="submit" className="btn btn-primary my-2 my-sm-0">
-                    <FormattedMessage id='project.global.buttons.search'/>
-                </button>
+        </form>
 
-            </form>
-
-        );
-
-    }
+    );
 
 }
 
