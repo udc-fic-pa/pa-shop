93b2af5b234ad965a76fa055c9063ae94250650d
Fernando <fbellas@udc.es>
2020-01-10 14:20:31 +0100

    Frontend: ShoppingItemList.js migrado a componente función.

--- frontend/src/modules/shopping/components/ShoppingItemList.js
+++ frontend/src/modules/shopping/components/ShoppingItemList.js
@@ -1,88 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {FormattedMessage, FormattedNumber} from 'react-intl';
 
 import ShoppingItem from './ShoppingItem';
 import {Errors} from '../../common';
 
-const initialState = {
-    backendErrors: null
-};
+const ShoppingItemList = ({list, edit, onUpdateQuantity, onRemoveItem}) => {
 
-class ShoppingItemList extends React.Component {
+    const [backendErrors, setBackendErrors] = useState(null);
 
-    constructor(props) {
-
-        super(props);
-
-        this.state = initialState;
-
-    }
-
-    setBackendErrors(backendErrors) {
-        this.setState({backendErrors});
-    }
-
-    handleErrorsClose() {
-        this.setState({backendErrors: null});
-    }
-
-    render() {
-
-        const {list, edit, onUpdateQuantity, onRemoveItem} = this.props;
-
-        if (list.items.length === 0) {
-            return (
-                <div className="alert alert-info" role="alert">
-                    <FormattedMessage id='project.shopping.ShoppingCart.empty'/>
-                </div>
-            );
-        }
-
+    if (list.items.length === 0) {
         return (
-
-            <div>
-
-                <Errors errors={this.state.backendErrors}
-                    onClose={() => this.handleErrorsClose()}/>
-
-                <table className="table">
-
-                    <thead>
-                        <tr>
-                            <th scope="col" style={{width: '60%'}}></th>
-                            <th scope="col" style={{width: '20%'}}>
-                                <FormattedMessage id='project.global.fields.price'/>
-                            </th>
-                            <th scope="col" style={{width: '20%'}}>
-                                <FormattedMessage id='project.global.fields.quantity'/>
-                            </th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {list.items.map(item => 
-                            <ShoppingItem shoppingItemListId={list.id}
-                                key={item.productId} item={item}
-                                edit={edit} 
-                                onUpdateQuantity={onUpdateQuantity}
-                                onRemoveItem={onRemoveItem}
-                                onBackendErrors={errors => this.setBackendErrors(errors)}/>
-                        )}
-                    </tbody>
-
-                </table>
-
-                <p className="text-center font-weight-bold">
-                    <FormattedMessage id='project.global.fields.totalPrice'/> 
-                    : <FormattedNumber value={list.totalPrice}/>€
-                </p>
-
+            <div className="alert alert-info" role="alert">
+                <FormattedMessage id='project.shopping.ShoppingCart.empty'/>
             </div>
-
         );
-
     }
+
+    return (
+
+        <div>
+
+            <Errors errors={backendErrors}
+                onClose={() => setBackendErrors(null)}/>
+
+            <table className="table">
+
+                <thead>
+                    <tr>
+                        <th scope="col" style={{width: '60%'}}></th>
+                        <th scope="col" style={{width: '20%'}}>
+                            <FormattedMessage id='project.global.fields.price'/>
+                        </th>
+                        <th scope="col" style={{width: '20%'}}>
+                            <FormattedMessage id='project.global.fields.quantity'/>
+                        </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {list.items.map(item => 
+                        <ShoppingItem shoppingItemListId={list.id}
+                            key={item.productId} item={item}
+                            edit={edit} 
+                            onUpdateQuantity={onUpdateQuantity}
+                            onRemoveItem={onRemoveItem}
+                            onBackendErrors={errors => setBackendErrors(errors)}/>
+                    )}
+                </tbody>
+
+            </table>
+
+            <p className="text-center font-weight-bold">
+                <FormattedMessage id='project.global.fields.totalPrice'/> 
+                : <FormattedNumber value={list.totalPrice}/>€
+            </p>
+
+        </div>
+
+    );
 
 }
 
