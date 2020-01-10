e9d0321e47f3880d3a3ac1655cf85e00f36b9bce
Fernando <fbellas@udc.es>
2020-01-10 14:07:02 +0100

    Frontend: ShoppingItem.js migrado a componente función.

--- frontend/src/modules/shopping/components/ShoppingItem.js
+++ frontend/src/modules/shopping/components/ShoppingItem.js
@@ -1,109 +1,84 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {FormattedMessage, FormattedNumber} from 'react-intl';
 
 import {ProductLink} from '../../common';
 
-class ShoppingItem extends React.Component {
+const ShoppingItem = ({shoppingItemListId, item, edit, onUpdateQuantity, 
+    onRemoveItem, onBackendErrors}) => {
 
-    constructor(props) {
+    const [quantity, setQuantity] = useState(item.quantity);
+    let form;
 
-        super(props);
-
-        this.state = {
-            quantity: props.item.quantity
-        };
-
-    }
-
-    handleQuantityChange(event) {
-        this.setState({quantity: Number(event.target.value)});
-    }
-
-    handleSubmit(event) {
+    const handleSubmit = event => {
 
         event.preventDefault();
 
-        if (this.form.checkValidity()) {
-            this.handleUpdateQuantity();
+        if (form.checkValidity()) {
+            
+            onUpdateQuantity(shoppingItemListId,
+                item.productId, quantity, 
+                () => onBackendErrors(null), 
+                backendErrors => {
+                    setQuantity(item.quantity);
+                    onBackendErrors(backendErrors);
+                });
+
         } else {
-            this.props.onBackendErrors(null);
-            this.form.classList.add('was-validated');
+
+            onBackendErrors(null);
+            form.classList.add('was-validated');
+
         }
     
     }
 
-    handleRemoveItem() {
+    const handleRemoveItem = () => {
 
-        this.props.onRemoveItem(this.props.shoppingItemListId,
-            this.props.item.productId, 
-            () => {
-                this.props.onBackendErrors(null);
-            }, 
-            backendErrors => {
-                this.props.onBackendErrors(backendErrors);
-            });
+        onRemoveItem(shoppingItemListId,
+            item.productId, 
+            () => onBackendErrors(null), 
+            backendErrors => onBackendErrors(backendErrors));
 
     }
 
-    handleUpdateQuantity() {
-
-        this.props.onUpdateQuantity(this.props.shoppingItemListId,
-            this.props.item.productId, this.state.quantity, 
-            () => {
-                this.setState({quantity: this.props.item.quantity});
-                this.props.onBackendErrors(null);
-            }, 
-            backendErrors => {
-                this.setState({quantity: this.props.item.quantity});
-                this.props.onBackendErrors(backendErrors);
-            });
-
-    }
-
-    render() {
-
-        const {item, edit} = this.props;
-
-        return (
-            <tr>                   
-                <td>
-                    { edit &&
-                    <span>
-                        <button type="button" className="btn btn-danger btn-sm"
-                            onClick={() => this.handleRemoveItem()}>
-                            <span className="fas fa-trash-alt"></span>
-                        </button>
-                        &nbsp;
-                        &nbsp;
-                    </span>
-                    }
-                    <ProductLink id={item.productId} name={item.productName}/>
-                </td>
-                <td><FormattedNumber value={item.productPrice}/>€</td>
+    return (
+        <tr>                   
+            <td>
                 { edit &&
-                <td>
-                    <form ref={node => this.form = node} 
-                        className="form-inline needs-validation" 
-                        noValidate onSubmit={(e) => this.handleSubmit(e)}>
-                        <input type="number" className="form-control mr-2" style={{width: '50%'}}
-                            value={this.state.quantity}
-                            onChange={(e) => this.handleQuantityChange(e)}
-                            min="1"/>
-                        <button type="submit" className="btn btn-primary">
-                            <FormattedMessage id="project.global.buttons.save"/>
-                        </button>
-                        <div className="invalid-feedback">
-                            <FormattedMessage id='project.global.validator.incorrectQuantity'/>
-                        </div>
-                    </form>
-                </td>
+                <span>
+                    <button type="button" className="btn btn-danger btn-sm"
+                        onClick={() => handleRemoveItem()}>
+                        <span className="fas fa-trash-alt"></span>
+                    </button>
+                    &nbsp;
+                    &nbsp;
+                </span>
                 }
-                {!edit && <td>{item.quantity}</td>}
-            </tr>
-        );
-
-    }
+                <ProductLink id={item.productId} name={item.productName}/>
+            </td>
+            <td><FormattedNumber value={item.productPrice}/>€</td>
+            { edit &&
+            <td>
+                <form ref={node => form = node} 
+                    className="form-inline needs-validation" 
+                    noValidate onSubmit={e => handleSubmit(e)}>
+                    <input type="number" className="form-control mr-2" style={{width: '50%'}}
+                        value={quantity}
+                        onChange={e => setQuantity(Number(e.target.value))}
+                        min="1"/>
+                    <button type="submit" className="btn btn-primary">
+                        <FormattedMessage id="project.global.buttons.save"/>
+                    </button>
+                    <div className="invalid-feedback">
+                        <FormattedMessage id='project.global.validator.incorrectQuantity'/>
+                    </div>
+                </form>
+            </td>
+            }
+            {!edit && <td>{item.quantity}</td>}
+        </tr>
+    );
 
 }
 
