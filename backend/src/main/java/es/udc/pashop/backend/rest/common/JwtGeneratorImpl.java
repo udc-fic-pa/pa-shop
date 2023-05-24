7c87818f19c594e2f77398d243d888a662826447
Fernando <fbellas@udc.es>
2023-05-24 13:44:29 +0200

    [Backend] Reordenación de imports en JwtGeneratorImpl.

--- backend/src/main/java/es/udc/pashop/backend/rest/common/JwtGeneratorImpl.java
+++ backend/src/main/java/es/udc/pashop/backend/rest/common/JwtGeneratorImpl.java
@@ -2,13 +2,13 @@ package es.udc.pashop.backend.rest.common;
 
 import java.util.Date;
 
-import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtGeneratorImpl implements JwtGenerator {
