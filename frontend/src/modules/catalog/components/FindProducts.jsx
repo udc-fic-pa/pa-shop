ae955715a660e40a4f7045d1b025172f05a39a96
Fernando <fbellas@udc.es>
2024-10-08 19:21:32 +0200

    [Frontend] Cambio menor a FindProducts.jsx.

--- frontend/src/modules/catalog/components/FindProducts.jsx
+++ frontend/src/modules/catalog/components/FindProducts.jsx
@@ -20,6 +20,7 @@ const FindProducts = () => {
 
         event.preventDefault();
         dispatch(actions.clearProductSearch());
+        navigate('/catalog/find-products-result');
 
         const criteria = {categoryId: toNumber(categoryId), 
             keywords: keywords.trim(), page: 0};
@@ -27,7 +28,6 @@ const FindProducts = () => {
 
         if (response.ok) {
             dispatch(actions.findProductsCompleted({criteria, result: response.payload}));
-            navigate('/catalog/find-products-result');
         }
 
     }
