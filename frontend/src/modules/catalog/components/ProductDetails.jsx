841048c1034d77b2e463858ecc155947d03846e5
Fernando <fbellas@udc.es>
2025-04-04 14:03:17 +0200

    [Frontend] Migración del componente ProductDetails a react-bootstrap.

--- frontend/src/modules/catalog/components/ProductDetails.jsx
+++ frontend/src/modules/catalog/components/ProductDetails.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react';
 import {useSelector} from 'react-redux';
 import {FormattedMessage, FormattedNumber} from 'react-intl';
 import {useParams} from 'react-router';
+import Card from 'react-bootstrap/Card';
 
 import users from '../../users';
 import * as selectors from '../selectors';
@@ -38,24 +39,24 @@ const ProductDetails = () => {
         
     return (
 
-        <div>
+        <>
 
             <BackLink/>
             
-            <div className="card text-center">
-                <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
-                    <h6 className="card-subtitle text-muted">
+            <Card className="text-center">
+                <Card.Body>
+                    <Card.Title>{product.name}</Card.Title>
+                    <Card.Subtitle className="text-muted">
                         <FormattedMessage id='project.global.fields.department'/>:&nbsp;
                             {selectors.getCategoryName(categories, product.categoryId)}
-                    </h6>
-                    <p className="card-text">{product.description}</p>
-                    <p className="card-text font-weight-bold">
+                    </Card.Subtitle>
+                    <Card.Text>{product.description}</Card.Text>
+                    <Card.Text className="fw-bold">
                         <FormattedMessage id='project.global.fields.price'/>{': '}
                         <FormattedNumber value={product.price} style="currency" currency="EUR"/>
-                    </p>
-                </div>
-            </div>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
 
             {loggedIn && 
                 <div>
@@ -64,7 +65,7 @@ const ProductDetails = () => {
                 </div>
             }
 
-        </div>
+        </>
 
     );
 
